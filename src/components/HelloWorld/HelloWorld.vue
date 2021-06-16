cda68926002733ccc5074423b2113c274dd1de2e
MeGallin <me@garyallin.uk>
2021-06-16 11:12:03 +0100

    ga/setup: basic setup inclding routing

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Home Component',
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <section id="home">
+    <p>This is the home page</p>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  data() {
+    return {
+      name: 'Home',
+    };
+  },
+  components: {},
+};
+</script>
+<style scoped src="./Home.css"></style>
