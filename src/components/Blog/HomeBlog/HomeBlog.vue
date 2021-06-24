7bb1fda432361d49c787cfb2f700c14b6e75f662
MeGallin <me@garyallin.uk>
2021-06-24 11:00:56 +0100

    ga/route-guards: workimg

--- src/components/Blog/HomeBlog/HomeBlog.vue
+++ src/components/Blog/HomeBlog/HomeBlog.vue
@@ -1,5 +1,6 @@
 <template>
   <div v-if="blogs !== '0 results'">
+    {{ userData }} {{ isAuthenticated }}
     <div v-for="blog in blogs" :key="blog.id">
       <h1>{{ blog.heading }}</h1>
       <p>{{ blog.message }}</p>
@@ -14,7 +15,7 @@ import $Store from '../../../store/index';
 import { mapGetters } from 'vuex';
 export default {
   computed: {
-    ...mapGetters(['blogs']),
+    ...mapGetters(['blogs', 'userData', 'isAuthenticated']),
   },
   created() {
     $Store.dispatch('getBlogs');
--- src/components/LoginForm/LoginForm.vue
+++ src/components/LoginForm/LoginForm.vue
@@ -2,11 +2,7 @@
   <div>
     <fieldset class="fieldSet">
       <legend>login form</legend>
-      <form
-        id="loginForm"
-        @submit.prevent
-        @submit="handleLogin(email, password)"
-      >
+      <form id="loginForm" @submit.prevent @submit="handleLogin(email, pwd)">
         <div>
           <label for="email"
             >E-mail
@@ -14,44 +10,57 @@
           </label>
         </div>
         <div>
-          <label for="password"
+          <label for="pwd"
             >Password
-            <input
-              type="password"
-              name="password"
-              id="password"
-              v-model="password"
-            />
+            <input type="password" name="pwd" id="pwd" v-model="pwd" />
           </label>
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <button type="submit" @click="counter()">Submit</button>
         </div>
       </form>
     </fieldset>
+
+    <div v-if="this.count > 0">
+      <h3>Error</h3>
+      <p>Either your email or password is in-correct.</p>
+      <p>Please try again.</p>
+    </div>
   </div>
 </template>
 
 <script>
 import $Store from '../../store/index';
+import { mapGetters } from 'vuex';
+
 export default {
   data() {
     return {
       email: '',
-      password: '',
+      pwd: '',
+      count: null,
     };
   },
+  computed: {
+    ...mapGetters(['isAuthenticated', 'userData']),
+  },
   methods: {
-    handleLogin(email, password) {
+    handleLogin(email, pwd) {
       this.email = email;
-      this.password = password;
+      this.pwd = pwd;
 
       const loginData = {
         email: this.email,
-        password: this.password,
+        pwd: this.pwd,
       };
 
       $Store.dispatch('userLogin', loginData);
+      this.email = '';
+      this.pwd = '';
+    },
+    counter() {
+      this.count += 1;
+      console.log(this.count);
     },
   },
 };
