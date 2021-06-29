55cb9ecd48db2964882d5fc1d74153f73559371c
MeGallin <me@garyallin.uk>
2021-06-29 10:05:12 +0100

    ga/minor style tweaks

--- src/components/Blog/HomeBlog/HomeBlog.vue
+++ src/components/Blog/HomeBlog/HomeBlog.vue
@@ -1,10 +1,12 @@
 <template>
   <div v-if="blogs !== '0 results'">
     <div v-for="blog in blogs" :key="blog.id" class="item">
-      <h1>{{ blog.heading }}</h1>
+      <h1 class="underline-dark">{{ blog.heading }}</h1>
       <p>{{ blog.message }}</p>
-      <p>{{ blog.posted }}</p>
-      <p>Post by {{ blog.name }}</p>
+      <div class="blog-footer-wrapper">
+        <p>{{ blog.posted }}</p>
+        <p>Post by {{ blog.name }}</p>
+      </div>
     </div>
   </div>
   <div v-else>
