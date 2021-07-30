82bd9a63c2b63ae57e1769948f2d7ac5eec87ddd
MeGallin <me@garyallin.uk>
2021-07-30 13:01:32 +0100

    updated files

---
+++ public/api/registrationEmail.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+$postdata = file_get_contents("php://input");
+
+if(isset($postdata) && !empty($postdata)) {
+    $request = json_decode($postdata, true); 
+
+     $name = $request['name'];
+     $surname = $request['surname'];
+     $email = $request['email'];
+     $pwd = $request['pwd'];
+
+     if(!empty($name) && !empty($surname) && !empty($email) && !empty($pwd)){
+        $message = $name." ".$surname. ", registered with this email address:\r\n ".$email. "\r\n and password: ". $pwd;
+     }else{
+        $message = "This message is only visible to the garyallin.uk It is a contact request message from your website from " .$name. " and email address: " .$email;
+     }    
+     $to = $email;
+     $subject = "Registration Request";     
+     $headers = "From:".$name."\r\n";
+     $headers .= "BCC: me@garyallin.uk".PHP_EOL;
+     $headers .= "Reply-To:".$email;
+
+    mail( $to, $subject, $message, $headers);
+}
+?>
